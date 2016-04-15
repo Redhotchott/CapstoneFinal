5464f3f94db6d90446b12f4bf8b37a47985e20d5
Redhotchott <t.chott@yahoo.com>
2016-04-15 12:00:53 -0600

    Friday Updates 1

--- SimpleSvm.R
+++ SimpleSvm.R
@@ -1,43 +1,34 @@
+rm(list=ls())
+
 library( 'e1071' )
 library('rpart')
-rm(list=ls())
+library('dplyr')
+setwd('/Users/tchott/Documents/CapstoneFinal')
 load('Predictors.Rdata')
-
 
 ptype.fac<-as.factor(ptype)
 
-Twb.type<-cbind(Twb.prof,ptype.fac)
+Twb.type<-cbind(Twb.prof,ptype.fac) %>% as.data.frame
+colnames(Twb.type)<-c("H0","H1","H2", "H3","H4","H5","H6","H7", "H8","H9","H10","H11","H12","H13","H14","H15","H16","H17","H18","H19","H20","H21","H22","H23","H24","H25","H26","H27","H28","H29","H30","ptype.df")
+attach(Twb.type)
 
 index <- 1:nrow(Twb.type)
-testindex <- sample(index, trunc(length(index/3)))
-testset<-Twb.type[testindex,]
-trainset<-Twb.type[-testindex,]
+set.seed(12)
+indexes <- sample(index, 200)
+testset <- Twb.type[indexes[1:100],]
+trainset <- Twb.type[indexes[101:200],]
+#class.weights
+###TUNE! LOOK INTO IT> 
+model<- svm( ptype.df~., data=trainset, probability=T, type='C-classification',class.weights=c("1"=1, "3"=2, "4"=8))
+plot(model, trainset, H0~H1, slice=list(H3=270,H4=270), xlim=c(270,280),fill=T, ylim=c(270,280))
+
+res <- predict( model, newdata=testset)
+head(res)
+table(pred = res, true = testset[,32])
+
+tune.out <- tune(svm, ptype.df~., data = trainset, probabilty=T,ranges = list(gamma=c(0.1,0.5,1,2,4),kernel=c("radial", "linear", "sigmoid"),  cost = c(0.1,1,10,100,1000)), class.weights= c("0" = 1, "1" = 10))
+
+unique(ptype.df)
 
 
-model<- svm( ptype.fac~., data=trainset, cost=100, gamma=1)
-res <- predict( model, newdata=test)
-
-
-
-
-
-
-
-
-data( iris )
-model <- svm( iris$Species~., iris )
-res <- predict( model, newdata=iris )
-
-library(e1071)
-library(rpart)
-data(Glass, package="mlbench")
-## split data into a train and test set
-index <- 1:nrow(Glass)
-testindex <- sample(index, trunc(length(index)/3))
-testset <- Glass[testindex,]
-trainset <- Glass[-testindex,]
-
-
-## svm
-svm.model <- svm(Type ~ ., data = trainset, cost = 100, gamma = 1)
-svm.pred <- predict(svm.model, testset[,-10])
+tune.out <- tune(svm, RESPONSE~., data = train, kernel="radial", ranges = list(gamma=c(0.1,0.5,1,2,4), cost = c(0.1,1,10,100,1000)), class.weights= c("0" = 1, "1" = 10))
